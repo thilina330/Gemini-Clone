8b2b0f4384702b097343f95a31e4f3a86a6f86c8
Thilina Lakshan <126488588+thilina330@users.noreply.github.com>
2024-06-30 20:04:55 +0530

    create app.js

---
+++ Gemini-Clone/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import Sidebar from "./components/Sidebar/Sidebar"
+
+
+
+function App() {
+
+
+  return (
+    <>
+      <Sidebar/>
+    </>
+  )
+}
+
+export default App
