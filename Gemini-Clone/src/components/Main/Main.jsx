97cba28a8ddafd562e247322d920f8c8d806beea
Thilina Lakshan <126488588+thilina330@users.noreply.github.com>
2024-07-02 17:12:20 +0530

    update main.jsx

--- Gemini-Clone/src/components/Main/Main.jsx
+++ Gemini-Clone/src/components/Main/Main.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
 import './main.css'
 import { assets } from '../../assets/assets'
+import { Context } from '../../context/Context'
+
 
 const Main = () => {
+
+  const {onSent, recentPrompt,showResult,loading,resultData,setInput,input }= useContext(Context)
+
   return (
     <div className='main'>
       <div className="nav">
@@ -10,9 +15,12 @@ const Main = () => {
         <img src={assets.R} alt="" />
       </div>
       <div className="main-container">
+        {!showResult? 
+        <>
+        
         <div className="greet">
             <p><span>Hello, Thilina.</span></p>
-            <p>How can I help you today?</p>
+            <p className='title'>How can I help you today?</p>
         </div>
         <div className="cards">
             <div className="card">
@@ -32,14 +40,29 @@ const Main = () => {
                <img src={assets.code_icon} alt="" />
             </div>
         </div>
+        
+        </>
+        
+        : <div className='result'>
+            <div className="result-title">
+              <img src={assets.R} alt="" />
+              <p>{recentPrompt}</p>
+            </div>
+          </div>}
+        
 
         <div className="main-bottom">
             <div className="search-box">
-                <input type="text" placeholder='Enter a prompt here' />
+                <input 
+                  onChange={(e) => setInput(e.target.value)} value={input}
+                  type="text" 
+                  placeholder='Enter a prompt here' 
+                  
+                />
                 <div>
                     <img src={assets.gallery_icon} alt="" />
                     <img src={assets.mic_icon} alt="" />
-                    <img src={assets.send_icon} alt="" />
+                    <img onClick={() => onSent()} src={assets.send_icon} alt="" />
                 </div>
             </div>
             <p className="bottom-info">
