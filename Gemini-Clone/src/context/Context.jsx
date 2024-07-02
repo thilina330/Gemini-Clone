55cb23282798e16fe8dc736abbdbcf028bb4c1f9
Thilina Lakshan <126488588+thilina330@users.noreply.github.com>
2024-07-02 17:08:53 +0530

    create context file

---
+++ Gemini-Clone/src/context/Context.jsx
@@ -0,0 +1,51 @@
+import { createContext, useState } from "react";
+import run from '../config/Gemini'
+
+export const Context = createContext();
+
+const ContextProvider = (props) =>{
+
+    const [input, setInput] = useState("")
+    const [recentPrompt, setRecentPrompt] = useState("")
+    const [prevePrompt, setPrevePrompt] = useState([])
+    const [showResult, setShowResult] = useState(false)
+    const [loading, setLoading] = useState(false)
+    const [resultData, setResultData] = useState("")
+
+    const onSent = async(prompt) => {
+       setResultData(""),
+       setLoading(true),
+       setShowResult(true) 
+       setRecentPrompt(input)
+       const response = await run(input)
+       setResultData(response)
+       setLoading(false)
+       setInput("")
+    }
+
+    //onSent("hello gpt")
+
+    const contextValue = {
+       
+        prevePrompt,
+        setPrevePrompt,
+        onSent,
+        setRecentPrompt,
+        recentPrompt,
+        showResult,
+        loading,
+        resultData,
+        input,
+        setInput
+    }
+
+    return(
+        <Context.Provider value={contextValue}>
+            {props.children}
+        </Context.Provider>
+
+    )
+}
+
+
+export default ContextProvider
