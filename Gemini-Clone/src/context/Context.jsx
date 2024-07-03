0fbf0d6ea9491d2c159936adb07346fc2bb06fd7
Thilina Lakshan <126488588+thilina330@users.noreply.github.com>
2024-07-03 12:54:59 +0530

    update context file

--- Gemini-Clone/src/context/Context.jsx
+++ Gemini-Clone/src/context/Context.jsx
@@ -12,13 +12,51 @@ const ContextProvider = (props) =>{
     const [loading, setLoading] = useState(false)
     const [resultData, setResultData] = useState("")
 
+    const delayPara = (index, nextWord) =>{
+    
+        setTimeout(function(){
+            setResultData(prev => prev+nextWord)
+
+        },75*index)
+    }
+
+    const newChat = () => {
+        setLoading(false)
+        setShowResult(false)
+    }
+
     const onSent = async(prompt) => {
        setResultData(""),
        setLoading(true),
        setShowResult(true) 
-       setRecentPrompt(input)
-       const response = await run(input)
-       setResultData(response)
+       let response;
+       if(prompt !== undefined){
+          response = await run(prompt)
+          setRecentPrompt(prompt)
+
+       }
+       else{
+        setPrevePrompt(prev => [...prev, input])
+        setRecentPrompt(input)
+        response = await run(input)
+       }
+      
+       let responseArray = response.split("**");
+       let newResponce = "";
+       for(let i=0 ; i<responseArray.length;i++){
+        if(i === 0 || i%2 !== 1){
+            newResponce += responseArray[i];
+        }
+        else{
+            newResponce += "<b>" + responseArray[i] + "<b/>";
+        }
+       }
+       let newResponce01 = newResponce.split('*').join('<br/>')
+       let newResponceArray = newResponce01.split(" ")
+       for(let i=0;i<newResponceArray.length;i++){
+        const nextWord = newResponceArray[i];
+         delayPara(i,nextWord+" ")
+       }
        setLoading(false)
        setInput("")
     }
@@ -36,7 +74,8 @@ const ContextProvider = (props) =>{
         loading,
         resultData,
         input,
-        setInput
+        setInput,
+        newChat
     }
 
     return(
